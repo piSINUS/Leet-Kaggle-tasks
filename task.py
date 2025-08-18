9e6717b90162a22381efe64f257e33911a2ca081
piSINUS <104928495+piSINUS@users.noreply.github.com>
2025-08-18 22:13:01 +0300

    Add files via upload

--- task.py
+++ task.py
@@ -256,31 +256,52 @@
 # print(f"The letter combinations for the digits '{test_digits}' are: {result}")
 
 # 21. Merge Two Sorted Lists
-class ListNode(object):
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
-class Solution(object):
-    def mergeTwoLists(self, list1, list2):
-        dummy = ListNode(0)
-        current = dummy
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+# class Solution(object):
+#     def mergeTwoLists(self, list1, list2):
+#         dummy = ListNode(0)
+#         current = dummy
         
-        while list1 and list2:
-            if list1.val < list2.val:
-                current.next = list1
-                list1 = list1.next
-            else:
-                current.next = list2
-                list2 = list2.next
-            current = current.next
+#         while list1 and list2:
+#             if list1.val < list2.val:
+#                 current.next = list1
+#                 list1 = list1.next
+#             else:
+#                 current.next = list2
+#                 list2 = list2.next
+#             current = current.next
         
-        if list1:
-            current.next = list1
-        elif list2:
-            current.next = list2
+#         if list1:
+#             current.next = list1
+#         elif list2:
+#             current.next = list2
         
-        return dummy.next
+#         return dummy.next
 # list1= [1,2,4]
 # list2 = [1,3,4]
 # result = sol.mergeTwoLists(list1,list2)
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        mapping = {')': '(', '}': '{', ']': '['}
+        stack = []
+
+        for char in s:
+            if char in mapping.values():
+                stack.append(char)
+            elif char in mapping:
+                if not stack or mapping[char] != stack.pop():
+                    return False
+        return not stack
+                
+sol = Solution()
+
+s = "([])"
+s1 = '(}'
+res = sol.isValid(s)
+print(res)
+        
 
