d8918da154356cba283c709bcdc39065cc898dd8
piSINUS <104928495+piSINUS@users.noreply.github.com>
2025-08-20 23:00:29 +0300

    Add files via upload

--- task.py
+++ task.py
@@ -306,22 +306,57 @@
         
 #  26 Remove Duplicates from Sorted Array
 
-nums = [0,0,1,1,1,2,2,3,3,4]
-class Solution(object):
-    def removeDuplicates(self, nums):
-        if not nums:
-            return 0
+# nums = [0,0,1,1,1,2,2,3,3,4]
+# class Solution(object):
+#     def removeDuplicates(self, nums):
+#         if not nums:
+#             return 0
         
-        unique_index = 0
-        
-        for i in range(1, len(nums)):
-            if nums[i] != nums[unique_index]:
-                unique_index += 1
-                nums[unique_index] = nums[i]
-        
-        return unique_index + 1
+#         unique_index = 0
 
-sol = Solution()
-result = sol.removeDuplicates(nums)
-print(f"The number of unique elements is: {result}")
-print(f"The modified array is: {nums[:result]}")
+#         for i in range(1,len(nums)):
+#             if nums[i] != nums[unique_index]:
+#                 unique_index += 1
+#                 nums[unique_index] = nums[i]
+#         return unique_index + 1
+    
+# sol = Solution()
+# result = sol.removeDuplicates(nums)
+# print(f"The number of unique elements is: {result}")
+# print(f"The modified array is: {nums[:result]}")
+
+# 18 4SUm
+
+class Solution(object):
+    def fourSum(self, nums, target):
+        nums.sort()
+        result = []
+        n = len(nums)
+        
+        for i in range(n - 3):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+            
+            for j in range(i + 1, n - 2):
+                if j > i + 1 and nums[j] == nums[j - 1]:
+                    continue
+                
+                left, right = j + 1, n - 1
+                
+                while left < right:
+                    current_sum = nums[i] + nums[j] + nums[left] + nums[right]
+                    
+                    if current_sum < target:
+                        left += 1
+                    elif current_sum > target:
+                        right -= 1
+                    else:
+                        result.append([nums[i], nums[j], nums[left], nums[right]])
+                        while left < right and nums[left] == nums[left + 1]:
+                            left += 1
+                        while left < right and nums[right] == nums[right - 1]:
+                            right -= 1
+                        left += 1
+                        right -= 1
+        
+        return result
